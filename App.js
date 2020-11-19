89b717c14b5d75aa4281e3ee9707583c0d487668
mablds <marcelobragalemos@gmail.com>
2020-11-19 03:23:36 -0300

    fix style

--- App.js
+++ App.js
@@ -6,14 +6,13 @@ import Home from './src/pages/home/Home'
 import Pokedex from './src/pages/pokedex/Pokedex'
 import Abilities from './src/pages/abilities/Abilities'
 import Types from './src/pages/types/Types'
-import { Header } from 'react-native/Libraries/NewAppScreen';
 
 const Stack = createStackNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Pokedex" component={Pokedex}/>
         <Stack.Screen name="Abilities" component={Abilities}/>
