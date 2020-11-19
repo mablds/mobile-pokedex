cc4751196b865dc00855c637de5a506384610589
mablds <marcelobragalemos@gmail.com>
2020-11-19 03:10:23 -0300

    install navigation stack

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Text, SafeAreaView, Image, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 const Home = () => {
     return (
